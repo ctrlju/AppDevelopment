34fec97c0f64c535000b55248875689009c1d2d4
ctrlju <julianaferreiralp@outlook.com>
2024-06-29 22:45:05 -0300

    FIxing SVG errors

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,9 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (async () => {
-    const config = await getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
 
-    const { transformer, resolver } = config;
+defaultConfig.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg');
+defaultConfig.resolver.sourceExts.push('svg');
 
-    config.transformer = {
-        ...transformer,
-        babelTransformerPath: require.resolve("react-native-svg-transformer"),
-    };
-    config.resolver = {
-        ...resolver,
-        assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-        sourceExts: [...resolver.sourceExts, "svg"],
-    };
-
-    return config;
-})();
+module.exports = defaultConfig;
